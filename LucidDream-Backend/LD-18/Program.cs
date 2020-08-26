e1bc1ee596633926079cd4bc84bde72ff989cb08
DavidDeres <david.deres@gmail.com>
2020-08-26 14:33:58 +0300

    added lists for active and non active channels

--- LucidDream-Backend/LD-18/Program.cs
+++ LucidDream-Backend/LD-18/Program.cs
@@ -5,10 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using RabbitMQ.Client;
 
+/* every time the user presses 'Accept', it will be translated to a list of all channels we
+ * want to activate.
+ * The main program will use direct exchange via RabbitMQ service and will send the checked
+ * channels via routing_key "ON" message*/
+
 namespace LD_18
 {
-    class Controller
-    {
+    class Controller {
+
+        static private List<ChannelNames> activeList = new List<ChannelNames>(),
+                                   nonActiveList = new List<ChannelNames>();
+
+ 
         static void Main(string[] args)
         {
             UserInput input = new UserInput();
@@ -16,9 +25,19 @@ namespace LD_18
             {
                 WelcomeMessage.WriteMessage();
                 var channelsList = input.GetChannelsToActivate(); // input from user 
+                // all channels are non active by default
+                nonActiveList = Enum.GetValues(typeof(ChannelNames)).Cast<ChannelNames>().ToList();
                 foreach (ChannelNames channel in channelsList) {
-                    //send "ON"  to routing_key "channel" by using Enum.ToString(channel)
+                    
                     Console.WriteLine($"{channel}"); // TESTING only
+
+                    activeList.Add(channel);                     // update checked channels
+
+                    nonActiveList.Remove(channel);               // update non-checked channels
+
+                    /* send "OFF" to all channels that weren't checked - in nonActiveList
+                    *send "ON"  to all channels in activeList Enum.ToString(channel)*/
+
                 }
                 //var body = Encoding.UTF8.GetBytes(message); // create a body to send via xchange
             }
--- LucidDream-Backend/LD-18/UserInput.cs
+++ LucidDream-Backend/LD-18/UserInput.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LD_18
 {
     class UserInput
