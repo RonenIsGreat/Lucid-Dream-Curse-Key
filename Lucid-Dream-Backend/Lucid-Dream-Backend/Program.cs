86c08a89aba48eabc2ca4b12e97c36db21b4d20f
Or <tur2620@gmail.com>
2020-08-26 15:23:34 +0300

    Finished pipleine for handling saving data. starting to create unit tests now

--- Lucid-Dream-Backend/Lucid-Dream-Backend/Program.cs
+++ Lucid-Dream-Backend/Lucid-Dream-Backend/Program.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
+using SaveStream;
 
 namespace Lucid_Dream_Backend
 {
     class Program
     {
+        static SaveStreamHelper saveStreamHelper;
+        static byte[] testData;
         static void Main(string[] args)
         {
+            int test = 0;
+            testData = new byte[1400];
+            for (int i = 0; i < testData.Length; i++)
+            {
+                var num = Encoding.UTF8.GetBytes(test.ToString());
+                testData[i] = num[0];
+            }
+            saveStreamHelper = new SaveStreamHelper();
+
+            Timer testTimer = new Timer(1024 / 10)
+            {
+                AutoReset = true
+            };
+            testTimer.Elapsed += TestTimer_Elapsed;
+            testTimer.Start();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        private static  void TestTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            saveStreamHelper.saveData(testData, "TestFile");
         }
     }
 }
---
+++ Lucid-Dream-Backend/SaveStreamHelper/SaveStreamHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+
+namespace SaveStream
+{
+    public class SaveStreamHelper
+    {
+        private ActionBlock<char[]> saveToFileBlock;
+        private BufferBlock<byte[]> dataBufferBlock;
+        private readonly string savePath;
+        private TransformBlock<byte[], char[]> transformDataBlock;
+        private string saveFileName;
+        public SaveStreamHelper()
+        {
+            //Get the recordings save path for the config file
+            this.savePath = ConfigurationManager.AppSettings["save-path"];
+            InitilaizeDataBlocks();
+        }
+
+        private void InitilaizeDataBlocks()
+        {
+            dataBufferBlock = new BufferBlock<byte[]>();
+            transformDataBlock = new TransformBlock<byte[], char[]>((data) =>
+            {
+                var encodedData = Encoding.UTF8.GetChars(data);
+                //TODO: transform data to saveable data here if needed
+                return encodedData;
+            });
+            saveToFileBlock = new ActionBlock<char[]>( (data) =>
+            {
+               ByteArrayToFile(savePath + '/' + saveFileName, data);
+               var readedBytes = File.ReadAllLines(savePath + '/' + saveFileName, Encoding.UTF8);
+                
+                Console.WriteLine("Saved file: " + saveFileName);
+            });
+            dataBufferBlock.LinkTo(transformDataBlock);
+            transformDataBlock.LinkTo(saveToFileBlock);
+        }
+
+        private bool ByteArrayToFile(string path, char[] byteArray)
+        {
+            try
+            {
+                using (var fs = new StreamWriter(path, true, Encoding.UTF8))
+                {
+                    fs.WriteLine(byteArray);
+                    fs.Flush();
+                    fs.Dispose();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+                return false;
+            }
+        }
+
+        public bool saveData(byte[] data, string saveFileName)
+        {
+            this.saveFileName = saveFileName;
+            try
+            {
+                dataBufferBlock.Post(data);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: "+e.Message);
+            }
+            return false;
+        }
+
+    }
+}
