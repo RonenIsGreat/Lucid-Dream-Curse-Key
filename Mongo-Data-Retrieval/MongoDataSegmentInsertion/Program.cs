765a2aac48a9b1e8d52fa260192a1ef7d727abf5
Ziv Krimberg <ziv.krim@gmail.com>
2020-09-14 17:20:58 +0300

    A program to retrieve range of data from MongoDB
    MongoDataSegmentInsertion is a dummy data for inserting hardcoded data
    into MongoDB for testing the MongoFetchByTime application.

---
+++ Mongo-Data-Retrieval/MongoDataSegmentInsertion/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDataSegmentInsertion
+{
+    static class Constants
+    {
+        public const int SegmentSize = 1400;
+        public const string DatabaseName = "LD-Database";
+        public const int CasNumMessagesInSecond = 9766;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] sampleDataArray = new byte[Constants.SegmentSize];
+
+            var client = new MongoClient("mongodb://127.0.0.1:27017");
+
+            string channel = GetChannel();
+            DateTime date = new DateTime(DateTime.Parse("01/01/2015 08:00:00").Ticks, DateTimeKind.Utc);
+            //DateTime date = DateTimeOffset.FromUnixTimeSeconds(1420099200).DateTime;
+            for (int i=0; i < (9766 * 10); i++)
+            {
+                //string seconds = i < 10 ? "0" + i.ToString() : i.ToString();
+                
+                SetUpSampleData(sampleDataArray);
+
+                InsertDocToCollection(client, channel, sampleDataArray, date);
+
+                date = date.AddTicks(1024);
+
+                //Console.WriteLine($"[{i}] Finished inserting to database, Length: {sampleDataArray.Length}");
+            }
+            
+            Console.ReadLine();
+        }
+
+        private static string GetChannel()
+        {
+            Console.WriteLine("Enter name of channel:");
+            return Console.ReadLine();
+        }
+
+        private static void SetUpSampleData(byte[] byteArray)
+        {
+            Random rnd = new Random();
+            rnd.NextBytes(byteArray);
+        }
+
+        private static void InsertDocToCollection(MongoClient client, string collectionName, byte[] byteArray, DateTime date)
+        {
+            var collection = client.GetDatabase(Constants.DatabaseName).GetCollection<BsonDocument>(collectionName);
+
+            var document = new BsonDocument
+            {
+                { "_date", date },
+                { "data", byteArray }
+            };
+
+            collection.InsertOne(document);
+        }
+    }
+}
---
+++ Mongo-Data-Retrieval/MongoFetchByTime/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace MongoFetchByTime
+{
+    static class Constants
+    {
+        public const string MongoURI = "http://localhost:8080/";
+        public const string FilterStart = "?filter={$and:[{\"_date\":{\"$gt\":{\"$date\":";
+        public const string FilterMiddle = "}}},{\"_date\":{\"$lt\":{\"$date\":";
+        public const string FilterEnd = "}}}]}&pagesize=1000&page=";
+    }
+
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            int countRes = 0;
+
+            String encodedAuth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes("admin:secret"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("basic", encodedAuth);
+            
+            Console.WriteLine("Enter Channel Name:");
+            string channel = Console.ReadLine();
+
+            Console.WriteLine("Enter First Date:");
+            DateTime date1 = new DateTime(DateTime.Parse(Console.ReadLine()).Ticks, DateTimeKind.Utc);
+            Console.WriteLine("Enter Second Date:");
+            DateTime date2 = new DateTime(DateTime.Parse(Console.ReadLine()).Ticks, DateTimeKind.Utc);
+
+            client.BaseAddress = new Uri(Constants.MongoURI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string query = channel + Constants.FilterStart + ((DateTimeOffset)date1).ToUnixTimeMilliseconds() + Constants.FilterMiddle
+                + ((DateTimeOffset)date2).ToUnixTimeMilliseconds() + Constants.FilterEnd;
+
+            string resultsAsString = "";
+            int page = 1;
+
+            using (var watch = new SuperWatch())
+            {
+                for( ; page <= 98 ; page++)
+                {
+                    try
+                    {
+                        //Console.WriteLine($"query: {query}");
+                        HttpResponseMessage response = await client.GetAsync($"{query}{page.ToString()}");
+                        if (response.IsSuccessStatusCode)
+                        {
+                            resultsAsString = await response.Content.ReadAsStringAsync();
+                        }
+
+                        JArray results = JArray.Parse(resultsAsString);
+
+                        //foreach (var res in results)
+                        //{
+
+                        //    DateTime resDate = DateTimeOffset.FromUnixTimeMilliseconds((long)res["_date"]["$date"]).DateTime;
+                        //    Console.WriteLine($"ID: {res["_id"]["$oid"]}, Date: {resDate.ToString()}");
+                        //    ++countRes;
+                        //}
+
+                        //Console.WriteLine($"Number of results: {countRes}");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
