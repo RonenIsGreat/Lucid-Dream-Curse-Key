10ffa0d7b7dd2c84cc4b9dc561212f84608dae0b
Ziv Krimberg <ziv.krim@gmail.com>
2020-09-17 11:01:52 +0300

    Added support of socketIO to GUI

--- sonar-status-display/src/App.js
+++ sonar-status-display/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
 import './App.scss';
 
 function App() {
+  const ENDPOINT = "http://127.0.0.1:4000";
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("StatusSocketIO", data => {
+      const res = JSON.parse(data);
+      setResponse(res);
+    });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
