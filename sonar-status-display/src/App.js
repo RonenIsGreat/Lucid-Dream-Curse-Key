eaa6110a1b5459110a774198d563992519cec395
karin <karinAzulay>
2020-09-17 13:22:24 +0300

    sonar display UI

--- sonar-status-display/src/App.js
+++ sonar-status-display/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.scss';
+import StatusBox from './components/StatusBox/StatusBox.comonent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+      <h1>Stream Array Status</h1>
+      <StatusBox name = {"Cas Beam Bas"} status = "channel-live"/>  
+      <br/> 
+      <StatusBox name = {"Cas Stave Bas"} status = {"channel-live"}/>  
+      <br/>  
+      <StatusBox name = {"Fas/Tas Beam Bas"} status = {"channel-live"}/>  
+      <br/>  
+      <StatusBox name = {"Fas/Tas Stave Bas"} status = {"channel-live"}/>  
+      <br/>  
+      <StatusBox name = {"PRS Beam Bas"} status = {"channel-dead"}/>  
+      <br/>  
+      <StatusBox name = {"ATM IDRS Bas"} status = {"channel-live"}/> 
+    </div>  
   );
 }
 
